1a50e41c307715ba5c6a51da9bd1eb3929596942
Pranali Prakash Mejari <pranali13630@mastek.com>
2020-06-05 01:46:05 +0100

    new build

---
+++ react-pwa-covid-19/build/precache-manifest.1b1c643590ee0b3e6adc06fc48f2508c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01a24025c833b5dba8c97c133c989703",
+    "url": "/index.html"
+  },
+  {
+    "revision": "964e2f2e65c909c0800f",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "bd917406ed308eaa9996",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "964e2f2e65c909c0800f",
+    "url": "/static/js/2.2ee0eafd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.2ee0eafd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd917406ed308eaa9996",
+    "url": "/static/js/main.40a2c18c.chunk.js"
+  },
+  {
+    "revision": "73725d60f849c2b09e30",
+    "url": "/static/js/runtime-main.6b241b14.js"
+  }
+]);
